f406ae619ca00f1e770fd4b4d0817440b92aaa43
Patrick <pbentley07@gmail.com>
2017-02-21 19:05:54 -0500

    first commit

---
+++ calc.js
@@ -0,0 +1,26 @@
+
+
+
+$("#button-1").click(function() {
+  alert("button clicked");
+});
+
+
+
+
+
+// Create two arrays(a and b) that will push our number selections into after clicked y user
+      // Join the array
+      // turn string to integer
+
+
+
+
+// Create operator array - push operator value to array
+      //if/else that will determing which mathematical operation is performed
+
+
+  // ON click function - Push Value into array and appending the value to the calculator panel #id
+
+
+//  if/ else - onclick
